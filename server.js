68244430b0f1d02e26fe89a421c2dd51b3e217db
Vlad <balashovv.vlad@gmail.com>
2021-10-01 15:08:12 +0300

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./dist/oblako-angular'))
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', {root: 'dist/oblako-angular'})
+})
